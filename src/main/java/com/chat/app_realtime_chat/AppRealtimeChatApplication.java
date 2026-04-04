9e86a7c83aaf2f23df2ca489ded0a91d4ba85631
FaizanAhmed2707 <farhan05faizan@gmail.com>
2026-04-04 17:27:22 +0530

    chacnged back to OG main runner

--- src/main/java/com/chat/app_realtime_chat/AppRealtimeChatApplication.java
+++ src/main/java/com/chat/app_realtime_chat/AppRealtimeChatApplication.java
@@ -1,28 +1,11 @@
 package com.chat.app_realtime_chat;
 
-import jakarta.servlet.MultipartConfigElement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.MultipartConfigFactory;
-import org.springframework.context.annotation.Bean;
-import org.springframework.util.unit.DataSize;
 
 @SpringBootApplication
 public class AppRealtimeChatApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AppRealtimeChatApplication.class, args);
-    }
-
-    // THIS BEAN FORCES THE TOMCAT SERVER TO ACCEPT FILES UP TO 50MB
-    // It overrides any conflicting settings injected by Azure's environment
-    @Bean
-    public MultipartConfigElement multipartConfigElement() {
-        MultipartConfigFactory factory = new MultipartConfigFactory();
-        // Set the absolute maximum size of a single uploaded file
-        factory.setMaxFileSize(DataSize.ofMegabytes(50));
-        // Set the absolute maximum size of the entire HTTP POST request
-        factory.setMaxRequestSize(DataSize.ofMegabytes(50));
-        return factory.createMultipartConfig();
+    public static void main(String[] args) {
+        SpringApplication.run(AppRealtimeChatApplication.class, args);
     }
 }
